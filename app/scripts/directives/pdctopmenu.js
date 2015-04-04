31ea77056cdbd162a8c1c0bdabe8401c34875645
Alexander Wiebe <Alexander.Wiebe@solvera.ca>
2015-04-03 21:47:00 -0600

    changed the directive view folder
    now its named partials

--- app/scripts/directives/pdctopmenu.js
+++ app/scripts/directives/pdctopmenu.js
@@ -9,11 +9,10 @@
 angular.module('pdc2015ResponsiveWebApp')
   .directive('pdcTopMenu', function () {
     return {
-      templateUrl: '/views/directives/pdctopmenu.html',
+      templateUrl: '/views/partials/pdctopmenu.html',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
         //element.text('this is the pdcTopMenu directive');
-        
       }
     };
   });
