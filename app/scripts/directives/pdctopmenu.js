589b896fc8c1c8f81aba24d8750ea28fd2f3a628
Alexander Wiebe <Alexander.Wiebe@solvera.ca>
2015-04-04 00:13:02 -0600

    pdc-top-menu is working
    wooooooooooooooo

--- app/scripts/directives/pdctopmenu.js
+++ app/scripts/directives/pdctopmenu.js
@@ -7,12 +7,17 @@
  * # pdcTopMenu
  */
 angular.module('pdc2015ResponsiveWebApp')
-  .directive('pdcTopMenu', function () {
+  .directive('pdcTopMenu', ['$location', function(location) {
     return {
       templateUrl: '/views/partials/pdctopmenu.html',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
+        scope.currentPage = location.path().substring(1).toLowerCase();
+        scope.setPage = function(selectedPage){
+          scope.currentPage = selectedPage;
+        };
+        //note: this uses the parent scope
         //element.text('this is the pdcTopMenu directive');
       }
     };
-  });
+  }]);
